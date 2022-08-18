0e02f39527f39324c95cacd18a33ac9bc49452ba
CTLM08 <c.t.liming@hotmail.com>
2022-08-18 20:41:47 +0800

    password

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,13 @@ import { ToastContainer, toast } from "react-toastify";
 
 import "react-toastify/dist/ReactToastify.css";
 import "animate.css";
+import pink from "./assets/pink.jfif";
+import slate from './assets/slate.jfif'
+import blue from './assets/blue.jfif'
 
 const App = () => {
   const [time, setTime] = useState(Date.now());
+  const [outline, setoutline] = useState(3);
 
   let bruh = 0;
 
@@ -19,7 +23,11 @@ const App = () => {
   }, []);
   const [num1, setnum] = useState("0");
   return (
-    <div className="bg-stone-300 overflow-y-hidden w-full h-screen flex items-center absolute justify-center">
+    <div
+      className={`overflow-y-hidden w-full h-screen flex items-center absolute justify-center ${
+        outline == 3 ? "bg-stone-300" : ""
+      } ${outline == 1 ? "bg-rose-300" : ""} ${outline == 2 ? "bg-blue-300" : ""}`}
+    >
       <ToastContainer
         position="top-right"
         autoClose={5000}
@@ -32,7 +40,11 @@ const App = () => {
         pauseOnHover
       />
 
-      <div className="w-64 md:w-auto   calculator_shadow2 relative z-[9999]">
+      <div
+        className={`w-64 md:w-auto   ${
+          outline == 3 ? "calculator_shadow2" : ""
+        } ${outline == 1 ? "pink2" : ""} ${outline == 2 ? "blue2" : ""} relative z-[9999]`}
+      >
         <div className="p-5">
           <div className="flex flex-row text-sm justify-between">
             <div className="gap-8 md:gap-14 flex flex-row">
@@ -54,16 +66,30 @@ const App = () => {
           <input
             value={num1}
             disabled
-            className="input2 w-full mt-3   rounded-xl h-10 md:h-20 flex items-center text-right p-5 text-2xl"
+            className={` w-full mt-3 rounded-xl h-10 md:h-20 flex items-center text-right p-5 text-2xl  ${outline==3 ?'input2':''} ${outline==1 ?'pink4':''} ${outline==2 ?'blue4':''} `}
           />
 
           <div className="mt-6 grid grid-cols-4 gap-4 text-xl">
-            <div className=" calculator_shadow3 w-full h-full flex items-center justify-center  aspect aspect-square">
+            <div
+              className={` ${outline == 3 ? "calculator_shadow3" : ""} ${
+                outline == 1 ? "pink3" : ""
+              }
+              ${
+                outline == 2 ? "blue3" : ""
+              }
+              w-full h-full flex items-center justify-center  aspect aspect-square`}
+            >
               <button className="" onClick={() => setnum("0")}>
                 AC
               </button>
             </div>
-            <div className="  calculator_shadow3  w-full h-full flex items-center justify-center  aspect aspect-square">
+            <div
+              className={` ${outline == 3 ? "calculator_shadow3" : ""} ${
+                outline == 1 ? "pink3" : ""
+              }${
+                outline == 2 ? "blue3" : ""
+              } w-full h-full flex items-center justify-center  aspect aspect-square`}
+            >
               <button
                 className=""
                 onClick={() =>
@@ -73,7 +99,13 @@ const App = () => {
                 <Icon icon="bi:backspace-fill" className="w-5 h-5" />
               </button>
             </div>
-            <div className="  calculator_shadow3  w-full h-full flex items-center justify-center  aspect aspect-square">
+            <div
+              className={`${outline == 3 ? "calculator_shadow3" : ""} ${
+                outline == 1 ? "pink3" : ""
+              } ${
+                outline == 2 ? "blue3" : ""
+              }  w-full h-full flex items-center justify-center  aspect aspect-square`}
+            >
               <button
                 className=""
                 onClick={() => {
@@ -83,7 +115,13 @@ const App = () => {
                 %
               </button>
             </div>
-            <div className=" calculator_shadow3 w-full h-full flex items-center justify-center  aspect aspect-square">
+            <div
+              className={`${outline == 3 ? "calculator_shadow3" : ""} ${
+                outline == 1 ? "pink3" : ""
+              }${
+                outline == 2 ? "blue3" : ""
+              }  w-full h-full flex items-center justify-center  aspect aspect-square`}
+            >
               <button
                 className=""
                 onClick={() => setnum(num1 == "0" ? "0" : num1 + "÷")}
@@ -133,7 +171,11 @@ const App = () => {
                     setnum((num1 == "0" ? "" : num1) + e);
                   }
                 }}
-                className={`mt-2 calculator_shadow3 w-full h-full flex items-center justify-center aspect aspect-square ${
+                className={`mt-2 ${outline == 3 ? "calculator_shadow3" : ""} ${
+                  outline == 1 ? "pink3" : ""
+                }${
+                  outline == 2 ? "blue3" : ""
+                } w-full h-full flex items-center justify-center aspect aspect-square ${
                   e == "=" && "row-span-2"
                 }`}
               >
@@ -152,7 +194,12 @@ const App = () => {
               className="bruh"
             >
               <path
-                fill="#a8a29e"
+                fill={`${outline == 3 ? "#a8a29e" : ""}  ${
+                  outline == 1 ? "#fb7185" : ""
+                }
+                ${
+                  outline == 2 ? "#3b82f6" : ""
+                }`}
                 fill-opacity="1"
                 d="M0,320L34.3,277.3C68.6,235,137,149,206,122.7C274.3,96,343,128,411,165.3C480,203,549,245,617,250.7C685.7,256,754,224,823,202.7C891.4,181,960,171,1029,192C1097.1,213,1166,267,1234,282.7C1302.9,299,1371,277,1406,266.7L1440,256L1440,320L1405.7,320C1371.4,320,1303,320,1234,320C1165.7,320,1097,320,1029,320C960,320,891,320,823,320C754.3,320,686,320,617,320C548.6,320,480,320,411,320C342.9,320,274,320,206,320C137.1,320,69,320,34,320L0,320Z"
               ></path>
@@ -163,7 +210,11 @@ const App = () => {
               className="bruh"
             >
               <path
-                fill="#a8a29e"
+                fill={`${outline == 3 ? "#a8a29e" : ""}  ${
+                  outline == 1 ? "#fb7185" : ""
+                }${
+                  outline == 2 ? "#3b82f6" : ""
+                }`}
                 fill-opacity="1"
                 d="M0,320L34.3,277.3C68.6,235,137,149,206,122.7C274.3,96,343,128,411,165.3C480,203,549,245,617,250.7C685.7,256,754,224,823,202.7C891.4,181,960,171,1029,192C1097.1,213,1166,267,1234,282.7C1302.9,299,1371,277,1406,266.7L1440,256L1440,320L1405.7,320C1371.4,320,1303,320,1234,320C1165.7,320,1097,320,1029,320C960,320,891,320,823,320C754.3,320,686,320,617,320C548.6,320,480,320,411,320C342.9,320,274,320,206,320C137.1,320,69,320,34,320L0,320Z"
               ></path>
@@ -174,15 +225,54 @@ const App = () => {
       <div className="absolute"></div>
       <Icon
         icon="charm:face-smile"
-        className="hover:animate-spin absolute left-2 top-1 h-32 w-32 md:h-96 md:w-96 text-stone-400 "
+        className={`hover:animate-spin absolute left-2 top-1 h-32 w-32 md:h-96 md:w-96 ${
+          outline == 3 ? " text-stone-400" : ""
+        } ${outline == 1 ? "text-rose-400" : ""} ${
+          outline == 2 ? "text-blue-400" : ""
+        }`}
       />
       <div
-        className="text-slate-500 absolute bottom-10 md:bottom-48 left-2 md:left-32 animate__animated animate__lightSpeedInLeft"
+        className={`${outline == 3 ? "text-slate-500" : ""} ${
+          outline == 1 ? "text-pink-600" : ""
+        } ${
+          outline == 2 ? "text-sky-700" : ""
+        } absolute bottom-10 md:bottom-48 left-2 md:left-32 animate__animated animate__lightSpeedInLeft"`}
         onClick={() => {
           bruh = 1;
         }}
       >
         made by LIMING 🧡
+      </div>
+      <div className="absolute flex flex-row gap-3 right-3 top-3 items-center">
+        <div className={` ${outline == 3 ? "text-slate-500" : ""} ${
+          outline == 1 ? "text-pink-600" : ""
+        } ${
+          outline == 2 ? "text-sky-700" : ""
+        }`}>color theme:</div>
+        <div
+          onClick={() => setoutline(1)}
+          className={`${
+            outline == 1 ? "border-2" : ""
+          } rounded-full border-slate-500`}
+        >
+          <img src={pink} className={`h-8 w-8 rounded-full `}></img>
+        </div>
+        <div
+          onClick={() => setoutline(2)}
+          className={`${
+            outline == 2 ? "border-2" : ""
+          } rounded-full border-slate-500`}
+        >
+          <img src={blue} className={`h-8 w-8 rounded-full `}></img>
+        </div>
+        <div
+          onClick={() => setoutline(3)}
+          className={`${
+            outline == 3 ? "border-2" : ""
+          } rounded-full border-black`}
+        >
+          <img src={slate} className={`h-8 w-8 rounded-full `}></img>
+        </div>
       </div>
     </div>
   );
